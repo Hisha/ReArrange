75e4d20b3c2da2b85801af82be79b1d82f244e77
Kevin Smith <ksmith@hishatech.com>
2015-02-09 09:26:09 -0500

    - Initial commit

---
+++ app/src/main/java/com/HishaTech/android/rearrange/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.HishaTech.android.rearrange;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by smithkev on 2/9/2015.
+ */
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
---
+++ app/src/main/java/com/HishaTech/android/rearrange/MainActivity.java
@@ -0,0 +1,36 @@
+package com.HishaTech.android.rearrange;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.About:
+                Intent Aboutintent = new Intent(this, AboutActivity.class);
+                this.startActivity(Aboutintent);
+                break;
+
+        }
+        return false; // should never happen
+    }
+}
